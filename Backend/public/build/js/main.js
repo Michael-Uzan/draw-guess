32d0d724f79c5b7561822c2c5edd1d55acde7bc1
Michael Uzan <looply3@gmail.com>
2022-01-08 16:41:47 +0200

    add react build to backend

--- Backend/public/build/js/main.js
+++
@@ -1,7 +0,0 @@
-if('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('../sw.js')
-            .then((reg) => console.log('Success: ', reg.scope))
-            .catch((err) => console.log('Failure: ', err));
-    })
-}
